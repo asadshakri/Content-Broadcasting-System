6066887723e4822eb11aa2ed015b6cdd701f7692
asadshakri <asadshakri3127@gmail.com>
2026-04-26 14:48:07 +0530

    Database connection

---
+++ src/app.js
@@ -0,0 +1,32 @@
+const express=require("express");
+const app=express();
+
+
+require("dotenv").config();
+
+const port=process.env.PORT;
+const mysql2=require("mysql2");
+const db=require("./utils/db-connection");
+
+
+
+db.sync({force:false}).then(()=>{
+    console.log("Database Synced Successfully");
+    app.listen(port,()=>{
+        console.log("Server is running on port "+port);
+    })
+}).catch((err)=>{
+    console.log("Unable to sync database",err);
+}
+)
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/utils/db-connection.js
@@ -0,0 +1,20 @@
+const {Sequelize}=require("sequelize");
+require("dotenv").config();
+
+const sequelize=new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASSWORD,{
+    host:process.env.DB_HOST,
+    dialect:process.env.DB_DIALECT,
+    logging:false
+})
+
+(async ()=>{
+    try{
+        await sequelize.authenticate();
+        console.log("Database Connected Successfully");
+    }
+    catch(err){
+        console.log("Unable to connect to the database",err);
+    }
+})();
+
+module.exports=sequelize;
